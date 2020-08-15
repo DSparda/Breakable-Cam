4a5c702069ff37b3a19a6e04909ff88226c5e1cc
Nguyen Duc Hung <dhungst1@gmail.com>
2020-08-16 03:00:01 +0700

    Add icons for takePictureFragment

--- app/src/main/java/com/example/breakablecam/screens/takingPicture/TakePictureViewModel.kt
+++ app/src/main/java/com/example/breakablecam/screens/takingPicture/TakePictureViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.breakablecam.screens.takingPicture
 
+import android.widget.ImageView
 import androidx.lifecycle.ViewModel
+import com.example.breakablecam.R
 
 class TakePictureViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun setStickerViewSource(imageView: ImageView?) {
+        imageView?.setImageResource(R.drawable.sticker)
+    }
+    fun setTakePhotoViewSource(imageView: ImageView?) {
+        imageView?.setImageResource(R.drawable.takephoto)
+    }
+    fun setMakeupViewSource(imageView: ImageView?) {
+        imageView?.setImageResource(R.drawable.makeup)
+    }
 }
